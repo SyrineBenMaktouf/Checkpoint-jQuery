d49f14d91e8a6a7cac02aed55a3a5fd5b20e492f
SyrineBenMaktouf <42380457+SyrineBenMaktouf@users.noreply.github.com>
2018-08-23 16:01:01 +0200

    Add files via upload

---
+++ CheckpointjQuery.js
@@ -0,0 +1,22 @@
+
+$("#btn1").click(function(){
+    $("#txt").css("fontWeight","Bold")})
+
+$("#btn2").click(function(){
+   $("#txt").css("fontStyle","Italic")})
+
+$("#btn3").click(function(){
+    $("#txt").css("fontSize", $('#btn3').val() + 'px')})
+
+$("#btn4").click(function(){
+    $("#txt").css("fontFamily",$('#btn4').val())})
+$("#btn5").click(function(){
+    $("#txt").css("textDecoration","underline")})
+$("#btn6").click(function(){
+   $("#txt").css("textDecoration","overline")})
+$("#btn7").click(function(){
+   $("#txt").css("color","Blue")})
+$("#btn8").click(function(){
+   $("#txt").css("textAlign","Justify")})
+    
+
